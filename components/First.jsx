24dca43a15ad4575c9a1957f60d0a0b8cdcb2c2f
enot2023 <soramjoykumar2023@gmail.com>
2024-03-18 22:22:51 +0530

    commit

--- components/First.jsx
+++ components/First.jsx
@@ -15,10 +15,10 @@ export default function First() {
       objectFit="cover" 
       objectPosition="center center"
     />
-      <div className="overlayContent pt-10">
+      <div className="overlayContent pt-24 lg:pt-10 ">
         <h1 className=" text-lg  sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-lime-500 fontb">IT Solutions that transform your business.</h1>
         <p className="font-a text-lg sm:text-md md:text-3xl lg:text-3xl fonta ">Leave the tech to us. Let&apos;s get started with yours</p>
-        <div className="text-sm sm:text-md md:text-lg lg:text-xlfont-bold mt-2 bg-clip-text flex items-center justify-center">
+        <div className="text-sm sm:text-md md:text-lg lg:text-xl font-bold mt-2  bg-clip-text flex items-center justify-center">
           <TypewriterEffect
             options={{
               strings: ["ARCHITECTURAL PLANNING AND DESIGN", "IT SERVICES"],
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="bg-lime-500 flex items-center justify-center">
       
       
-          <p className="   py-4  text-sm text-gray-500 lg:mt-0 dark:text-gray-400">
+          <p className="  px-4 lg:px-0 py-4  text-sm text-gray-500 lg:mt-0 dark:text-gray-400">
             © Copyright 2023. All rights reserved. HARMAT ENTERPRISES
           </p>
         </div>
