4f44e7b5cb9f6577a4dae96a935b21637efb7734
enot2023 <soramjoykumar2023@gmail.com>
2024-03-19 10:39:39 +0530

    commit

--- components/Mobilesidebar.tsx
+++ components/Mobilesidebar.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import MobileSiderbar from './Mobilesidebar';
-// import {menu} from "lucide-react"
-const Header = () => {
-    const Menu = [
+"use client"
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+import { Menu } from 'lucide-react'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+// hydration sembei
+const MobileSiderbar = () => {
+    const Menus = [
         {
           id: 1,
           name: 'HOME',
@@ -31,29 +36,30 @@ const Header = () => {
             path: '/',
         },
       ];
+    const [isMounted,setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+    if(!isMounted){
+        return null;
+    }
   return (
-    <div className='fixed w-full clearNav z-50 flex  item-center justify-between  p-4 lg:pl-24 shadow-sm bg-lime-500'>
-        <div className='flex items-center  gap-10 '>    
+    <Sheet >
+        <SheetTrigger  >
+        <Button variant='ghost' size='icon' className='md:hidden'>
+            <Menu />
+        </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className='p-0'>
+        <div className='grid grid-cols-2  gap-10 '>    
         <Link href={"/"}>
-        <Image alt='logo' src="/logo.png" width={60} height={60} /></Link>
-        </div>
-        <ul className='md:flex md:text-sm md:gap-2 lg:gap-8 hidden pt-7'>
-        {Menu.map((item) => (
-          <Link key={item.id}  href={item.path}>
-            <li className='hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out'>
-              {item.name}
-            </li>
-          </Link>
-          
-            ))}
-        </ul>
-
-         <div className='flex items-center justify-end'>
+        <Image alt='logo' src="/logo.png" width={60} height={60} className='ml-5 mt-3' /></Link>
+        <div className='flex items-center justify-center'>
          <a
             href="https://facebook.com/"
             rel="noopener noreferrer"
             target="_blank"
-            className="invisible md:visible"
+
           >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="30" height="20">
             <path fill="currentColor" d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
@@ -64,17 +70,31 @@ const Header = () => {
             href="https://instagram.com/"
             rel="noopener noreferrer"
             target="_blank"
-            className="invisible md:visible"
           >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="30" height="20">
             <path fill="currentColor" d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
             </svg>
           </a>
          </div>
-         <div className='flex items-center justify-end pr-5'><MobileSiderbar/></div>
-
-    </div>
+        </div>
+        <ul className='md:flex md:text-sm pt-7'>
+        {Menus.map((item) => (
+          <Link key={item.id}  href={item.path}>
+            <li className='flex items-center ml-2 mt-2 mb-2 hover:text-primary cursor-pointer text-pretty hover:scale-105  transition-all ease-in-out'>
+              {item.name}
+            </li>
+            <li>
+            <hr className="mt-3 border-gray-200 mx-2 " />
+            </li>
+          </Link>
+          
+            ))}
+        </ul>
+        
+        </SheetContent>
+    </Sheet>
+    
   )
 }
 
-export default Header
+export default MobileSiderbar
