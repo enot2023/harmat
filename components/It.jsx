d64c394f3f09f5477393c63212d9946f9cf7156e
enot2023 <soramjoykumar2023@gmail.com>
2024-03-20 11:53:15 +0530

    commit

--- components/It.jsx
+++ components/It.jsx
@@ -1,68 +1,179 @@
-
-import React from "react";
-import { motion } from "framer-motion";
+"use client"
 import Image from "next/image";
-import Link from "next/link";
+import React, { useEffect } from "react";
+import {Button} from "@/components/ui/button"
 const It = () => {
-  return (
-    <div className=" relative isolate ">
-      <div
-        className="absolute inset-x-0 -z-10 overflow-hidden blur-2xl hidden sm:block "
-        aria-hidden="true"
-      >
-        <div
-          className="relative  left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-screen-xl px-4 pt-8 sm:px-6 lg:px-8 ">
-        <h1 className="text-2xl  font-extrabold">
-          Seamless <span className="text-lime-500">IT</span> Enterprise{" "}
-          <span className="text-lime-500">Solutions</span>
-        </h1>
-        <h1 className=" pt-2">
-          Unleash Efficiency and Innovation. Elevate Your Business Today!
-        </h1>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 pt-10 mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 gap-10">
-  <div className="md:col-span-1">
-    <Link href={"/"} className="flex flex-col items-center justify-center image-containers hover:text-white">
-      <Image src={"/it/responsive.png"} width={70} height={70} alt="machi" />
-      <h1 className="font-semibold text-center">Web Development</h1>
-    </Link>
-    <Link href={"/"} className="pt-6 flex flex-col items-center justify-center image-containers hover:text-transparent">
-      <Image src={"/it/network.png"} width={70} height={70} alt="machi" />
-      <h1 className="font-semibold text-center">NETWORK SECURITY</h1>
-    </Link>
-    <Link href={"/"} className="pt-6 flex flex-col items-center justify-center image-containers hover:text-transparent">
-      <Image src={"/it/server.png"} width={70} height={70} alt="machi" />
-      <h1 className="font-semibold text-center">ASSET MANAGEMENT</h1>
-    </Link>
-  </div>
-  <div className="md:col-span-1 hidden md:flex justify-center"> {/* Hide on mobile */}
-    <Image src={"/source.gif"} alt="machi" width={500} height={500} className="roundedImage" />
-  </div>
-  <div className="md:col-span-1">
-    <Link href={"/"} className="flex flex-col items-center justify-center image-containers hover:text-transparent">
+ 
+  useEffect(() => {
+    let listItemDom = document.querySelector('.carousel .list');
+    let thumbnailDom = document.querySelector('.carousel .thumbnail');
+    let carouselDom = document.querySelector('.carousel');
+    let timeRunning = 4000;
+    let runTimeOut;
+    let activeIndex = 0;
 
-      <Image src={"/it/security.png"} width={70} height={70} alt="machi" />
-      <h1 className=" font-semibold text-center">Home Security</h1>
-    </Link>
-    <Link href={"/"} className="pt-6 flex flex-col items-center justify-center image-containers hover:text-white">
+    const showSlider = (type) => {
+      const itemSlider = document.querySelectorAll('.carousel .list .item');
+      const itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
       
-      <Image src={"/it/mri.png"} width={70} height={70} alt="machi" />
-      <h1 className="font-semibold text-center">Medical Equipment <br /> Related services</h1>
-    </Link>
-    <Link href={"/"} className="pt-6 flex flex-col items-center justify-center image-containers hover:text-white">
-      
-      <Image src={"/it/machinery.png"} width={70} height={70} alt="machi" />
-      <h1 className="font-semibold text-center">Industrial Equipment <br /> Related Service</h1>
-    </Link>
-  </div>
-</div>
+      if (type === 'next') {
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+      } else if (type === 'prev') {
+        listItemDom.prepend(itemSlider[itemSlider.length - 1]);
+        thumbnailDom.prepend(itemThumbnail[itemThumbnail.length - 1]);
+        carouselDom.classList.add('prev');
+      }
+
+      clearTimeout(runTimeOut);
+      runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('prev');
+        carouselDom.classList.remove('next');
+      }, timeRunning);
+    };
+
+    document.getElementById('prev').onclick = () => {
+      showSlider('prev');
+    };
+
+    document.getElementById('next').onclick = () => {
+      showSlider('next');
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="carousel ">
+        <div className="list">
+          <div className="item">
+            <div className="img"><Image src={"/it/web.jpg"}   width={1000} height={1000}/></div>
+            <div className="content">
+             
+              <div className="topic">Web Development</div>
+              <div className="des  ">At Harmat, we specialize in crafting cutting-edge web solutions tailored to meet the unique needs of your business. Our expert team of developers is dedicated to delivering high-quality, custom-built websites and web applications that drive results and exceed expectations.
+               <br />With a focus on user experience, performance, and scalability, we offer a comprehensive range of web development services. Whether you&apos;re looking to launch a brand new website, revamp an existing one, or develop a powerful web application, we have the expertise to bring your vision to life.</div>
+              <div className="buttons">
+                <Button className="bg-lime-500">See more</Button>
+         
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div  className="img"><Image src={"/it/networks.jpg"}  width={1000} height={1000}/>
+     </div>       <div className="content">
+             
+              <div className="topic">Network Security</div>
+              <div className="des " >At Hatman, we understand the critical importance of safeguarding your digital assets against ever-evolving cyber threats. Our comprehensive network security services are designed to protect your organization&apos;s sensitive information, maintain compliance with regulatory standards, and mitigate the risks associated with malicious attacks.</div>
+              <div className="buttons">
+                <Button className="bg-lime-500">See more</Button>
+              
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div  className="img"><Image src={"/it/asset.jpg"}  width={1000} height={1000}/>
+        </div>    <div className="content">
+              
+              <div className="topic">Asset Management</div>
+              <div className="des  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus, doloremque tempore animi id ipsam quos corporis perspiciatis, assumenda dolores modi rem  Reprehenderit!</div>
+              <div className="buttons">
+                <Button className="bg-lime-500">See more</Button>
+               
+              </div>
+            </div>
+          </div>
+         <div className="item">
+            <div  className="img"><Image src={"/it/homesecurity.jpg"}  width={1000} height={1000}/>
+ </div>           <div className="content">
+            
+              <div className="topic">Home Security</div>
+              <div className="des  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus, doloremque tempore animi id ipsam quos corporis perspiciatis, assumenda dolores modi rem quae voluptatem repudiandae! </div>
+              <div className="buttons">
+                <Button className="bg-lime-500" >See more</Button>
+              
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div  className="img"><Image src={"/it/medicalequipment.jpg"}  width={1000} height={1000}/></div>
+            <div className="content">
+              
+              <div className="topic">Medical Equipment related services</div>
+              <div className="des  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus, doloremque tempore animi id ipsam quos corporis perspiciatis, assumenda dolores modi rem quae voluptatem repudiandae! Nesciunt tempore pariatur quam ea quisquam fuga odio adipisci. Est,!</div>
+              <div className="buttons">
+                <Button className="bg-lime-500">See more</Button>
+             
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div  className="img"><Image src={"/it/industrialequi.jpg"}  width={1000} height={1000}/></div>
+            <div className="content">
+            
+              <div className="topic">Industrial Equipment Related Service</div>
+              <div className="des ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus, doloremque tempore animi id ipsam quos corporis perspiciatis, assumenda dolores modi rem quae voluptatem repudiandae! Nesciunt tempore pariatur quam ea quisquam fuga odio adipisci. Est, inventore nobis. Aspernatur soluta explicabo velit molestiae animi. Reprehenderit!</div>
+              <div className="buttons">
+                <Button  className="bg-lime-500">See more</Button>
+               
+              </div>
+            </div>
+          </div> 
+        </div>
+        {/* thumnail */}
+        <div className="thumbnail">
+         
+          <div className="item">
+          <div  className="img"><Image src={"/it/networks.jpg"}  width={1000} height={1000}/>
+     </div>       <div className="content">
+              <div className="title text-white">Network security </div>
+          </div>
+          </div>
+          <div className="item">
+          <div  className="img"><Image src={"/it/asset.jpg"} width={1000} height={1000}/>
+         </div>   <div className="content">
+              <div className="title text-white">Asset Management</div>
+              
+          </div>
+          </div>
+          <div className="item">
+          <div  className="img"><Image src={"/it/homesecurity.jpg"} width={1000} height={1000}/>
+  </div>          <div className="content">
+              <div className="title text-white">Home Security</div>
+              
+          </div>
+          </div> 
+          <div className="item">
+          <div  className="img"><Image src={"/it/medicalequipment.jpg"} width={1000} height={1000}/></div>
+            <div className="content">
+              <div className="title text-white">Medical Equipment related services</div>
+              
+          </div>
+          </div>
+          <div className="item">
+          <div  className="img"><Image src={"/it/industrialequi.jpg"} width={1000} height={1000}/>
+</div>            <div className="content">
+              <div className="title text-white">Industrial Equipment Related Service</div>
+              
+          </div>
+          </div>
+          <div className="item">
+          <div  className="img"><Image src={"/it/web.jpg"}  width={1000} height={1000}/>
+          </div>  <div className="content">
+              <div className="title text-white">Web Development</div>
+              
+          </div>
+          </div>
+
+        </div>
+       {/* awrows */}
+        <div className="arrows">
+          <Button id="prev" >←</Button>
+          <Button id="next" >→</Button>
+        </div>
+        <div className="time"></div>
+      </div>
+     
     </div>
   );
 };
--- components/about.tsx
+++ components/about.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 const About = () => {
   return (
     <div className='mb-4'>
-      <hr className="mt-10 border-gray-200 " />
+      {/* <hr className="mt-10 border-gray-200 " /> */}
       <h2 className='flex items-center justify-center mt-6 text-3xl font-semibold'>About Us</h2>
       <div className='grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 '>
         <div className='flex items-center justify-center gap-10 lg:gap-24 -mt-10 lg:mt-8'>
           <p  className='order-2 lg:order-1 text-center pr-4 lg:px-4'>Contact us at : <a href="mailto:contact@harmatenterprise.com">contact@harmatenterprise.com</a></p>
           <Image src={"/logo.png"} width={200} height={200} alt='Company Logo' className="order-1 lg:order-2 w-32 h-32 md:w-50 md:h-50 ml-4 md:ml-24" />
         </div>
-        <div className='flex items-center  justify-center mx-5 lg:mx-24 md:ml-24 font-serif font-semibold'>
+        <div className='flex items-center  justify-center mx-5 lg:mx-24 md:ml-24 font-serif text-sm text-gray-500'>
           <p>Nestled in Imphal, HARMAT ENTERPRISE PRIVATE LIMITED is a hub of architectural innovation, smart planning, and advanced IT networking. Our passion for creativity and technology fuels our progress. With a team from across India, we&apos;re not just changing spaces; we&apos;re making possibilities limitless. Welcome to a new era of excellence!</p>
         </div>
       </div>
