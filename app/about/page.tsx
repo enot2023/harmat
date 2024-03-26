7c10cf78fda0c314c3eab95708bd60777ec4913b
enot2023 <soramjoykumar2023@gmail.com>
2024-03-26 10:22:16 +0530

    commit-about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,14 +9,13 @@ const page = () => {
       <Image
       width={1000}
       height={1000}
-        className="object-cover object-center w-3/4 "
+        className="object-cover object-center w-3/4 qwerty"
         alt="Placeholder Image"
         src="/about/about.jpg"
-        style={{ width: '100vw', height: '100vh' }}
       />
       <div className="overlayContent">
       <div className='contentas'>
-            <h1 className=''>what we do</h1>
+            <h1 className=' text-[26px] md:text-[40px]'>what we do</h1>
             <p className='text-sm'>Architectural Planning & Design | School Management System | Smart Home | IT Solutions | ASSET MANAGEMENT</p>
             
         </div>
--- components/about/first.tsx
+++ components/about/first.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const First = () => {
   return (
-    <div className='mx-auto max-w-screen-xl pl-10 pt-8'>
-       <h1 className=" text-[40px] lg:text-[50px] xl:text-[70px]  font-sans font-light">
+    <div className='mx-auto max-w-screen-xl px-8 pt-8'>
+       <h1 className=" text-[38px] lg:text-[50px] xl:text-[70px]  font-sans font-light">
             WHAT MAKES US A SPECIAL AGENCY?
           </h1>
           <p className="text-[17px] pb-5 ">
@@ -14,10 +14,10 @@ const First = () => {
             <h1 className=" text-[40px] lg:text-[50px] xl:text-[70px]  font-sans font-light">
             Who we are?
           </h1>
-          <p className="text-[17px]  ">
+          <p className="text-[17px] md:pr-8 ">
           we are pioneers in IT services and architecture planning and design. With a relentless passion for innovation and a commitment to excellence, we empower businesses to harness the full potential of technology for sustainable growth and success.
           </p></div>
-          <div className='pb-5 mr-10 order-1 md:order-2'>
+          <div className='pb-5 order-1 md:order-2 md:pl-8'>
           <Image
             src="/about/who.jpg"
             height="1000"
@@ -28,7 +28,7 @@ const First = () => {
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className='pb-5 mr-10 '>
+        <div className='pb-5 md:pr-8'>
           <Image
             src="/about/who1.jpg"
             height="1000"
@@ -38,10 +38,10 @@ const First = () => {
           />
           </div>
           <div className=' pb-5'>
-            <h1 className=" text-[40px] lg:text-[50px] xl:text-[70px]  font-sans font-light">
+            <h1 className=" text-[40px] lg:text-[50px] xl:text-[70px]  font-sans font-light md:pl-8">
             Our Purpose
           </h1>
-          <p className="text-[17px]  ">
+          <p className="text-[17px] md:pl-8 ">
           our purpose is to empower businesses to thrive in the digital age by providing unparalleled IT services and architecting innovative solutions that drive sustainable growth and transformation.</p></div>
           
         </div>
